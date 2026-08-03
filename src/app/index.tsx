535455261bbdbde5877e4536bf21e15b96bec62e
abiel13 <dbestabi28@gmail.com>
2026-08-03 15:55:09 +0100

    chore: configure supabase integration

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,20 @@
+import { supabase } from "@/lib/supabase";
+import { useEffect } from "react";
 import { View, Text } from "react-native";
 
 export default function HomeScreen() {
+
+  useEffect(() => {
+    const testConnection = async () => {
+      const { data, error } = await supabase.auth.getSession();
+
+      console.log("Session:", data);
+      console.log("Error:", error);
+    };
+
+    testConnection();
+  }, []);
+
   return (
     <View
       style={{
